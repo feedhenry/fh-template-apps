d163d5eb39cb1d33d099b3dc8f762925542a18a5
Michael Nairn <mnairn@redhat.com>
2017-03-01 13:34:34 +0000

    RHMAP-13678 - Add new grunt tasks:
    girefs - Update git references in config files:
    
    $grunt gitrefs --git-ref refs/heads/FH-v9.9.x
    
    branch,tag,push - Branch/Tag and Push changes to template apps repos.
    
    $ grunt clean clone branch tag push --git-branch FH-v9.9.x --git-tag release-9.9.0

---
+++ tasks/branch.js
@@ -0,0 +1,36 @@
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  var templateHelper = require('./lib/templateHelper').init(grunt);
+  var _ = require('underscore');
+
+  function branchAppTemplates(filepath, branch) {
+    grunt.config.set('gitcheckout', {});
+
+    var globalJson = grunt.file.readJSON(filepath);
+
+    templateHelper.walkObjects(globalJson.show, function (obj) {
+      var prefix = 'gitcheckout.' + obj.id + '.options';
+      if (_.has(obj, 'repoUrl') && _.has(obj, 'repoBranch')) {
+        grunt.config.set(prefix + '.branch', branch);
+        grunt.config.set(prefix + '.create', true);
+        grunt.config.set(prefix + '.cwd', 'tmp/' + obj.id);
+      }
+    });
+    if (_.isEmpty(grunt.config.get('gitcheckout'))) {
+      grunt.log.ok("No git apps defined in " + filepath);
+    } else {
+      grunt.task.run('gitcheckout');
+    }
+  }
+
+  grunt.registerMultiTask('branch', 'Create a branch on all defined app repos', function () {
+    var branch = grunt.option('git-branch');
+    this.filesSrc.forEach(function (filepath) {
+      branchAppTemplates(filepath, branch);
+    });
+  });
+
+};
