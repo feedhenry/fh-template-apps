a6dea9f7444c55178be8cb19bf42a09d5e1aa74e
Michael Nairn <mnairn@redhat.com>
2017-03-01 13:37:47 +0000

    Update clone grunt task:
    * Update any github urls to use one that isn't read only (git@github.com).
    * Add clone-depth option(Default is 1).
    
    $ grunt clean clone --clone-depth=9999

--- tasks/clone.js
+++ tasks/clone.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
   var templateHelper = require('./lib/templateHelper').init(grunt);
   var _ = require('underscore');
 
-  function cloneAppTemplates(filepath) {
+  function cloneAppTemplates(filepath, cloneDepth) {
     grunt.config.set('gitclone', {});
 
     var globalJson = grunt.file.readJSON(filepath);
@@ -14,23 +14,24 @@ module.exports = function(grunt) {
     templateHelper.walkObjects(globalJson.show, function (obj) {
       var prefix = 'gitclone.' + obj.id + '.options';
       if (_.has(obj, 'repoUrl') && _.has(obj, 'repoBranch')) {
+        var repoUrl = obj.repoUrl.replace('git://github.com/','git@github.com:');
         grunt.config.set(prefix + '.directory', 'tmp/' + obj.id);
-        grunt.config.set(prefix + '.repository', obj.repoUrl);
+        grunt.config.set(prefix + '.repository', repoUrl);
         grunt.config.set(prefix + '.branch', obj.repoBranch.split('/').pop());
-        grunt.config.set(prefix + '.depth', 1);
+        grunt.config.set(prefix + '.depth', cloneDepth);
       }
     });
     if (_.isEmpty(grunt.config.get('gitclone'))) {
       grunt.log.ok("No git apps defined in " + filepath);
     } else {
-      grunt.task.run('gitclone')
+      grunt.task.run('gitclone');
     }
   }
 
   grunt.registerMultiTask('clone', 'Clone all defined app repos', function () {
-    grunt.task.requires('copy');
+    var cloneDepth = grunt.option('clone-depth') || 1;
     this.filesSrc.forEach(function (filepath) {
-      cloneAppTemplates(filepath);
+      cloneAppTemplates(filepath, cloneDepth);
     });
   });
 
