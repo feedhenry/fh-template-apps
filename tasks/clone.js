8d2bc7c3f5f233bfec23623efef6c428b284d813
Michael Nairn <mnairn@redhat.com>
2017-05-15 09:53:27 +0100

    Make using ssh optional during clone, default is to just use whatever is in the config files. (git://)

--- tasks/clone.js
+++ tasks/clone.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
   var templateHelper = require('./lib/templateHelper').init(grunt);
   var _ = require('underscore');
 
-  function cloneAppTemplates(filepath, cloneDepth) {
+  function cloneAppTemplates(filepath, cloneDepth, useSsh) {
     grunt.config.set('gitclone', {});
 
     var globalJson = grunt.file.readJSON(filepath);
@@ -14,7 +14,7 @@ module.exports = function(grunt) {
     templateHelper.walkObjects(globalJson.show, function (obj) {
       var prefix = 'gitclone.' + obj.id + '.options';
       if (_.has(obj, 'repoUrl') && _.has(obj, 'repoBranch')) {
-        var repoUrl = obj.repoUrl.replace('git://github.com/','git@github.com:');
+        var repoUrl = useSsh ? obj.repoUrl.replace('git://github.com/','git@github.com:') : obj.repoUrl;
         grunt.config.set(prefix + '.directory', 'tmp/' + obj.id);
         grunt.config.set(prefix + '.repository', repoUrl);
         grunt.config.set(prefix + '.branch', obj.repoBranch.split('/').pop());
@@ -30,8 +30,9 @@ module.exports = function(grunt) {
 
   grunt.registerMultiTask('clone', 'Clone all defined app repos', function () {
     var cloneDepth = grunt.option('clone-depth') || 1;
+    var useSsh = grunt.option('use-ssh');
     this.filesSrc.forEach(function (filepath) {
-      cloneAppTemplates(filepath, cloneDepth);
+      cloneAppTemplates(filepath, cloneDepth, useSsh);
     });
   });
 
